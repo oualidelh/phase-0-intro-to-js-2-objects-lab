dfad2215d60a541c6f156b76c1d6f3f67135cbd6
oualidelhouari <elhouarioualid@gmail.com>
2023-07-21 02:04:37 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name: "oualid",
+    streetAdress: "djelfa"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employees = { ...employee };
+    employees[key] = value;
+    return employees;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const employees = { ...employee };
+    delete employees[key];
+    return employees;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
